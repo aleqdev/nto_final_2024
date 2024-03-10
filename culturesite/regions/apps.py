73782cf0a80334f8b1089f881ae6f2735fa2c2b1
necromfox <necromfox@gmail.com>
2024-03-10 23:14:15 +0300

    123

--- culturesite/regions/apps.py
+++ culturesite/regions/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class RegionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "regions"
-    verbose_name = "Адресный реестр ХУЙ"
+    verbose_name = "Адресный реестр"
