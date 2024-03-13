2bdfa9343f785dd34fca999e368ecc69ae93fddb
maksonchik20 <elena0903@mail.ru>
2024-03-13 17:07:53 +0300

    merge

--- culturesite/education/apps.py
+++ culturesite/education/apps.py
@@ -4,8 +4,4 @@ from django.apps import AppConfig
 class EducationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'education'
-<<<<<<< HEAD
-    verbose_name = "Образовательная деятельность"
-=======
     verbose_name = 'Образовательная деятельность'
->>>>>>> 4214f858148c147eadac3574c88ede0eb22a9eac
--- culturesite/entertainment/admin.py
+++ culturesite/entertainment/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-from person.models import Person
+# from person.models import Person
 
-<<<<<<< HEAD
-admin.site.register(Person)
-=======
 from .models import Place
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     pass
->>>>>>> 4214f858148c147eadac3574c88ede0eb22a9eac
