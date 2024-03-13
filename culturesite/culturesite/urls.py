c74202659493a66075039e63e7971121db49744e
Your Name <you@example.com>
2024-03-13 16:36:50 +0300

    hui

--- culturesite/culturesite/urls.py
+++ culturesite/culturesite/urls.py
@@ -26,5 +26,5 @@ admin.site.enable_nav_sidebar = False
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
