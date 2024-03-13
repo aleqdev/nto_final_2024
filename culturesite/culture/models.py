1db536dd68cc9354d342b50ff412d46d90e10b5b
Your Name <you@example.com>
2024-03-13 17:03:55 +0300

    123'

--- culturesite/culture/models.py
+++ culturesite/culture/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Artifact(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    owner = models.ForeignKey(on_delete=models.PROTECT, verbose_name="Владелец")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Экспонат"
+        verbose_name_plural = "Экспонаты"
+
+
+class ArtifactOwner(models.Model):
+    is_study = models.BooleanField()
+    name = models.CharField(max_length=50, verbose_name="Название")
+    owner = models.ForeignKey(on_delete=models.PROTECT, verbose_name="Владелец")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Экспонат"
+        verbose_name_plural = "Экспонаты"
