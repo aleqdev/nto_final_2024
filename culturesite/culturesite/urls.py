946d9e9e9f1756d55353d0690c41281b754b7c01
Your Name <you@example.com>
2024-03-13 18:40:16 +0300

    assas

--- culturesite/culturesite/urls.py
+++ culturesite/culturesite/urls.py
@@ -25,12 +25,12 @@ admin.site.enable_nav_sidebar = False
 
 
 def index(request):
-    return redirect("/admin")
+    return redirect("admin")
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("^$", index)
+    path("admin/", admin.site.urls, name='admin'),
+    path("", index)
 ]
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
