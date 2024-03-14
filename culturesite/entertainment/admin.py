c4e432fab7a35e479c207f74448b9fa068f78600
Your Name <you@example.com>
2024-03-14 10:50:52 +0300

    23

--- culturesite/entertainment/admin.py
+++ culturesite/entertainment/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, EventType, Event
 from import_export.admin import ImportExportModelAdmin
 
 
 @admin.register(Place)
 class PlaceAdmin(ImportExportModelAdmin):
-    list_display = ["id", "name", "capacity"]
+    list_display = ["object", "id", "name", "capacity"]
+
+    def object(self, obj):
+        return str(obj)
+    
+    object.short_description = 'Объект'
+    
+
+@admin.register(EventType)
+class EventTypeAdmin(ImportExportModelAdmin):
+    list_display = ["type_name", "id"]
+    
+
+@admin.register(Event)
+class EventAdmin(ImportExportModelAdmin):
+    list_display = ["object", "id", "date", "name", "event_type", "datetime_begin", "datetime_end", "visitors_count", "place", "is_paid"]
+
+    def object(self, obj):
+        return str(obj)
+    
+    object.short_description = 'Объект'
     
--- culturesite/entertainment/models.py
+++ culturesite/entertainment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Place(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название")
     capacity = models.IntegerField(verbose_name="Вместимость") 
@@ -11,3 +12,33 @@ class Place(models.Model):
     class Meta:
         verbose_name = "Место"
         verbose_name_plural = "Места"
+
+
+class EventType(models.Model):
+    type_name = models.CharField(max_length=50, verbose_name="Тип мероприятия")
+
+    def __str__(self):
+        return self.type_name
+    
+    class Meta:
+        verbose_name = "Тип мероприятия"
+        verbose_name_plural = "Типы мероприятий"
+
+
+class Event(models.Model):
+    date = models.DateField(verbose_name="Дата проведения")
+    name = models.CharField(max_length=150, verbose_name="Название")
+    event_type = models.ForeignKey(EventType, on_delete=models.CASCADE, verbose_name="Тип")
+    datetime_begin = models.DateTimeField(verbose_name="Время начала")
+    datetime_end = models.DateTimeField(verbose_name="Время окончания")
+    visitors_count = models.PositiveIntegerField(verbose_name="Количество поситителей")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name="Пространство")
+    is_paid = models.BooleanField(verbose_name="Платное мероприятие")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.name} ({self.datetime_begin} - {self.datetime_end})"
+    
+    class Meta:
+        verbose_name = "Мероприятие"
+        verbose_name_plural = "Мероприятия"
