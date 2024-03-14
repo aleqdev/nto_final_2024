e41227b559c9c72d6fdce79e50e69a40acd39314
Your Name <you@example.com>
2024-03-14 13:57:43 +0300

    123

--- culturesite/culturesite/urls.py
+++ culturesite/culturesite/urls.py
@@ -33,5 +33,3 @@ urlpatterns = [
     path("admin/", admin.site.urls, name='admin'),
     path("", RedirectView.as_view(url=reverse_lazy("admin:index")))
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
