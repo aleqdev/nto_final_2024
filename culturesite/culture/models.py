8af7a9b059f61a7d7ab000f16d0f6a98ee7fcbb9
Your Name <you@example.com>
2024-03-14 16:40:41 +0300

    ВладельцыЭ п

--- culturesite/culture/models.py
+++ culturesite/culture/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from education.models import Study
 
 
-class Artifact(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Название")
-    owner = models.ForeignKey(Study, on_delete=models.PROTECT, verbose_name="Владелец")
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Экспонат"
-        verbose_name_plural = "Экспонаты"
-
-
 # Сторонние орг
 class ForeignOrganization(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
@@ -24,6 +12,25 @@ class ForeignOrganization(models.Model):
     class Meta:
         verbose_name = "Сторонняя организация"
         verbose_name_plural = "Сторонние организации"
+
+
+class Artifact(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    owner_study = models.ForeignKey(Study, on_delete=models.PROTECT, verbose_name="Владелец (Студия)", null=True, blank=True)
+    owner_foreign_organization = models.ForeignKey(ForeignOrganization, on_delete=models.PROTECT, verbose_name="Владелец (Сторонняя организация)", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def owner(self):
+        if self.owner_study is not None:
+            return self.owner_study
+        return self.owner_foreign_organization
+    owner.short_description = "Владелец"
+
+    class Meta:
+        verbose_name = "Экспонат"
+        verbose_name_plural = "Экспонаты"
 
 
 class ShowcaseType(models.Model):
