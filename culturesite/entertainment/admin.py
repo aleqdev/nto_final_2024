0155fb8e5e55cc1ed2b4668a0a52c73b6804ce98
Your Name <you@example.com>
2024-03-14 13:14:55 +0300

    123

--- culturesite/entertainment/admin.py
+++ culturesite/entertainment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Place, Location, EventType, Event, UnitPlace
+from .models import Place, Location, EventType, Event, UnitPlace, UnitPlacePurchase
 from import_export.admin import ImportExportModelAdmin
 
 
@@ -62,22 +62,67 @@ from django.core.validators import MinValueValidator
 
 
 class ChangePriceForm(ActionForm):
-    price = forms.IntegerField(required=True, validators=[MinValueValidator(0)], label="Цена")
+    price = forms.IntegerField(required=False, validators=[MinValueValidator(0)], label="Цена")
 
+
+class IsFree(admin.FieldListFilter):
+    title = 'is free'
+    parameter_name = 'is_free'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('True', 'Yes'), 
+            ('False', 'No')
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == 'True':
+            return queryset.exclude(external_number__exact='')
+        elif value == 'False':
+            return queryset.filter(external_number__exact='')
+        return queryset
+    
 
 @admin.register(UnitPlace)
 class UnitPlaceAdmin(ImportExportModelAdmin):
-    list_display = ["object", "price"]
+    list_display = ["object", "price", "is_free"]
+    list_filter = [("is_free", IsFree)]
     action_form = ChangePriceForm
-    actions = ["change_price"]
+    actions = ["change_price", "sell_ticket"]
+
+    def object(self, obj):
+        return str(obj)
+    object.short_description = 'Объект'
+
+    def change_price(modeladmin, request, queryset):
+        queryset.update(price=int(request.POST['price'] or '0'))
+    change_price.short_description = "Изменить цену"
+
+    def sell_ticket(modeladmin, request, queryset):
+        import datetime
+
+        print(123)
+
+        for item in queryset:
+            UnitPlacePurchase.objects.create(
+                datetime=datetime.datetime.now(),
+                event=item.event,
+                location=item.location,
+                row=item.row,
+                column=item.column,
+                price=item.price
+            )
+
+    sell_ticket.short_description = "Продать билеты"
+    
+
+@admin.register(UnitPlacePurchase)
+class UnitPlacePurchaseAdmin(ImportExportModelAdmin):
+    list_display = ["object", "id", "datetime", "location", "row", "column", "price"]
 
     def object(self, obj):
         return str(obj)
     
     object.short_description = 'Объект'
-
-    def change_price(modeladmin, request, queryset):
-        queryset.update(price=int(request.POST['price']))
-
-    change_price.short_description = "Изменить цену"
     
