df15d9c2d5538624774dc8e8a7ff6685bf013c51
Your Name <you@example.com>
2024-03-14 18:03:00 +0300

    123

--- culturesite/culture/models.py
+++ culturesite/culture/models.py
@@ -133,12 +133,6 @@ class ArtifactTransportAct(models.Model):
 
     def clean(self):
         pass
-<<<<<<< HEAD
-=======
-        # print("aba", self.showcase_order.showcase.type)
-        # print(self.artifacts.all().all())
->>>>>>> 142a607b4053a48c07c05e9b14b7304f774f3779
-
 
     class Meta:
         verbose_name = "Акт передачи экспонатов на выставку"
