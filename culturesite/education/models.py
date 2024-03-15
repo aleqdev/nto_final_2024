189d22b57b4cdf3e29c0a5eb6cfb9f0eadea4821
maksonchik20 <elena0903@mail.ru>
2024-03-15 10:28:55 +0300

    merge

--- culturesite/education/models.py
+++ culturesite/education/models.py
@@ -22,3 +22,25 @@ class TeacherEducation(models.Model):
     class Meta:
         verbose_name = "Преподаватель"
         verbose_name_plural = "Преподаватели"
+
+class Student(models.Model):
+    fio = models.CharField(verbose_name="ФИО посетителя")
+    def __str__(self):
+        return f"{self.fio}"
+    
+    class Meta:
+        verbose_name = "Посетитель центра"
+        verbose_name_plural = "Посетители центров"
+
+
+class ActInviteStudy(models.Model):
+    date = models.DateTimeField(verbose_name="Дата заявки")
+    act_study_start_order = models.ForeignKey(StudyStartOrder, on_delete=models.CASCADE, verbose_name="Приказ о работе студии")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name="Студент")
+
+    def __str__(self):
+        return f"Акт: {self.act_study_start_order}. Студент: {self.student}. Дата регистрации: {self.date.strftime("%Y-%m-%d %H:%M:%S")}"
+    
+    class Meta:
+        verbose_name = "Посетитель центра"
+        verbose_name_plural = "Посетители центров"
