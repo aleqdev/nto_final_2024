0b49b1a05d92363ed108c03a6c3bd43fea59b1c6
Your Name <you@example.com>
2024-03-15 11:04:27 +0300

    orders

--- culturesite/education/admin.py
+++ culturesite/education/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Study, TeacherEducation
+from .models import Study, TeacherEducation, StudyStartOrder
 from import_export.admin import ImportExportModelAdmin
 from .resources import StudiesResource
+from django import forms
+
 
 @admin.register(Study)
 class StudyAdmin(ImportExportModelAdmin):
@@ -12,3 +14,47 @@ class StudyAdmin(ImportExportModelAdmin):
 @admin.register(TeacherEducation)
 class TeacherEducationAdmin(ImportExportModelAdmin):
     pass
+
+
+class StudyStartOrderForm(forms.ModelForm):
+    class Meta:
+        model = StudyStartOrder
+        exclude = tuple()
+
+    def clean(self):
+        from django.forms import ValidationError
+
+        date_begin = self.cleaned_data['date_begin'] 
+        date_end = self.cleaned_data['date_end'] 
+        teacher = self.cleaned_data['teacher'] 
+        weekdays = self.cleaned_data['weekdays'] 
+        time_begin = self.cleaned_data['time_begin'] 
+        time_end = self.cleaned_data['time_end']
+
+        if date_begin > date_end:
+            raise ValidationError({
+                "date_end": "Дата окончания работы студии раньше даты начала работы!"
+            })
+        
+        if time_begin > time_end:
+            raise ValidationError({
+                "time_end": "Время окончания заятий раньше времени начала занятий!"
+            })
+        
+        if not teacher.is_free(date_begin,
+                               date_end,
+                               [w.id for w in weekdays.all()],
+                               time_begin,
+                               time_end
+                               ):
+            raise ValidationError({
+                    "weekdays": "Пересечение графика занятий!"
+                })
+            
+
+@admin.register(StudyStartOrder)
+class StudyStartOrderAdmin(ImportExportModelAdmin):
+    list_display = ["study", "datetime", "date_begin", "date_end", "teacher", "time_begin", "time_end"]
+    form = StudyStartOrderForm
+
+
--- culturesite/education/models.py
+++ culturesite/education/models.py
@@ -19,13 +19,37 @@ class TeacherEducation(models.Model):
     def __str__(self):
         return f"{self.fio}"
     
+    def is_free(self, date_begin, date_end, weekdays_ids, time_begin, time_end):
+        weekdays = Weekday.objects.filter(id__in=weekdays_ids)
+        orders = StudyStartOrder.objects.filter(date_begin__gte=date_begin, date_end__lte=date_end, teacher=self)
+
+        for order in orders:
+            for order_weekday in order.weekdays.all():
+                for weekday in weekdays:
+                    if order_weekday.id == weekday.id:
+                        if not (
+                            (order.time_begin <= time_begin and order.time_end <= time_begin) or
+                            (order.time_begin >= time_end and order.time_end >= time_end)
+                        ):
+                            return False
+                        
+        return True
+
+    
     class Meta:
         verbose_name = "Преподаватель"
         verbose_name_plural = "Преподаватели"
 
 
 class Weekday(models.Model):
-    name = models.CharField(verbose_name="Название")
+    name = models.CharField(max_length=50, verbose_name="Название")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "День недели"
+        verbose_name_plural = "Дни недели"
 
 
 class StudyStartOrder(models.Model):
@@ -35,4 +59,9 @@ class StudyStartOrder(models.Model):
     date_end = models.DateField(verbose_name="Дата окончания работы")
     teacher = models.ForeignKey(TeacherEducation, on_delete=models.CASCADE, verbose_name="Преподаватель")
     weekdays = models.ManyToManyField(Weekday, verbose_name="Дни недели")
-    time = models.TimeField(verbose_name="Время занятий")
+    time_begin = models.TimeField(verbose_name="Время начала занятий")
+    time_end = models.TimeField(verbose_name="Время окончания занятий")
+
+    class Meta:
+        verbose_name = "Приказ о работе студии"
+        verbose_name_plural = "Приказы о работе студии"
