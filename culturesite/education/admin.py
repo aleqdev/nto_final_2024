f9570131e73e1d934cfee7876f85359c9dc7da99
Your Name <you@example.com>
2024-03-15 12:26:29 +0300

    123A

--- culturesite/education/admin.py
+++ culturesite/education/admin.py
@@ -4,6 +4,22 @@ from import_export.admin import ImportExportModelAdmin
 from .resources import StudiesResource
 from django import forms
 
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
+
+
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('Нажмите "открыть сайт" для отчётов')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('Нажмите "открыть сайт" для отчётов')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Нажмите "открыть сайт" для отчётов')
+
+admin_site = MyAdminSite()
+
 
 class ActInviteStudyInline(admin.StackedInline):
     model = ActInviteStudy
