4b5b8808d0049d8dc9422d5739585ed740bd19ca
Your Name <you@example.com>
2024-03-15 10:28:27 +0300

    d

--- culturesite/education/models.py
+++ culturesite/education/models.py
@@ -22,3 +22,17 @@ class TeacherEducation(models.Model):
     class Meta:
         verbose_name = "Преподаватель"
         verbose_name_plural = "Преподаватели"
+
+
+class Weekday(models.Model):
+    name = models.CharField(verbose_name="Название")
+
+
+class StudyStartOrder(models.Model):
+    datetime = models.DateTimeField(verbose_name="Дата формирования приказа")
+    study = models.ForeignKey(Study, on_delete=models.CASCADE, verbose_name="Студия")
+    date_begin = models.DateField(verbose_name="Дата начала работы")
+    date_end = models.DateField(verbose_name="Дата окончания работы")
+    teacher = models.ForeignKey(TeacherEducation, on_delete=models.CASCADE, verbose_name="Преподаватель")
+    weekdays = models.ManyToManyField(Weekday, verbose_name="Дни недели")
+    time = models.TimeField(verbose_name="Время занятий")
